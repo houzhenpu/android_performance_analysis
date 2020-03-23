8964ac7577daf47dea17f5acaecaab34478d1999
houzhenpu <houzhenpu@jiemian.com>
2020-03-23 11:18:50 +0800

    添加自定义callback注释

--- doraemonkit/src/main/java/com/performance/analysis/kit/timecounter/instrumentation/ProxyHandlerCallback.java
+++ doraemonkit/src/main/java/com/performance/analysis/kit/timecounter/instrumentation/ProxyHandlerCallback.java
@@ -7,11 +7,6 @@ import android.text.TextUtils;
 import com.performance.analysis.kit.timecounter.TimeCounterManager;
 import com.performance.analysis.util.Reflector;
 
-/**
- * @author: linjizong
- * @date: 2019/6/3
- * @desc:
- */
 class ProxyHandlerCallback implements Handler.Callback {
 
     private static final int LAUNCH_ACTIVITY = 100;
@@ -30,13 +25,18 @@ class ProxyHandlerCallback implements Handler.Callback {
 
     @Override
     public boolean handleMessage(Message msg) {
+        // 处理消息开始，同时返回消息类型，主要为了兼容Android P，把159消息转为101(Pause)和100(Launch)
         int msgType = preDispatch(msg);
+        // 如果旧的callback返回true,表示已经被它拦截，而它内部必定调用了Handler.handleMessage,直接返回
         if (mOldCallback != null && mOldCallback.handleMessage(msg)) {
             postDispatch(msgType);
             return true;
         }
+        // 直接调用handleMessage执行消息处理
         mHandler.handleMessage(msg);
+        // 处理消息结束
         postDispatch(msgType);
+        // 返回true,表示callback会拦截消息，Hanlder不需要再处理消息因为我们上一步已经处理过了
         return true;
     }
 
