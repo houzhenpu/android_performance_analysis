3c73fe771a00977cd74ba073bbaa89f0f0c9e8a0
houzhenpu <houzhenpu@jiemian.com>
2020-04-17 10:30:32 +0800

    性能参数对外回调实现

--- app/src/main/java/com/performance/doraemondemo/SecondActivity.java
+++ app/src/main/java/com/performance/doraemondemo/SecondActivity.java
@@ -2,12 +2,12 @@ package com.performance.doraemondemo;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.blankj.utilcode.util.ToastUtils;
 import com.didichuxing.doraemondemo.R;
 import com.performance.analysis.kit.timecounter.OnTimeCounterListener;
 import com.performance.analysis.kit.timecounter.TimeCounterManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -19,9 +19,11 @@ public class SecondActivity extends AppCompatActivity {
         TimeCounterManager.get().setOnTimeCounterListener(new OnTimeCounterListener() {
             @Override
             public void onTimeCounter(String counterInfo) {
+                System.out.println("counterInfo    " + counterInfo);
                 ToastUtils.showShort(counterInfo);
             }
         });
     }
 
 }
+
